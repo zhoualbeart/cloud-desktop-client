f0b641967c470db0a431ef1f4ea9cf4bd208554c
zhoualbeart <zhoualbeart@gmail.com>
2018-05-25 15:02:29 +0800

    update bg pic

--- Win32C++Lesson9/Win32Lesson9/Win32Lesson1/Resource.h
+++ Win32C++Lesson9/Win32Lesson9/Win32Lesson1/Resource.h
@@ -14,11 +14,12 @@
 #define IDR_MAINFRAME                   128
 #define IDI_ICON3                       131
 #define IDB_PNG1                        132
-#define IDB_PNG_BG                      132
 #define IDB_BITMAP1                     133
 #define IDB_PNG_CLOSE                   136
 #define IDB_PNG_CLOSE_PRESE             137
 #define IDB_PNG_CLOSE_PRESS             137
+#define IDB_PNG2                        139
+#define IDB_PNG_BG                      139
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -26,7 +27,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_RESOURCE_VALUE        140
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
